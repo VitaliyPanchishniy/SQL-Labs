bb94e52b6c2e666e4f3a7d0e8437517a4e022392
VitaliyPanchishniy <144204960+VitaliyPanchishniy@users.noreply.github.com>
2023-11-13 11:53:11 +0200

    Add files via upload

---
+++ laba7.sql
@@ -0,0 +1,22 @@
+CREATE TABLE Categories (
+    category_id INT PRIMARY KEY AUTO_INCREMENT,
+    category_name VARCHAR(50) NOT NULL
+);
+
+CREATE TABLE News (
+    news_id INT PRIMARY KEY AUTO_INCREMENT,
+    category_id INT,
+    title VARCHAR(100) NOT NULL,
+    content TEXT NOT NULL,
+    publication_date TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    FOREIGN KEY (category_id) REFERENCES Categories(category_id)
+);
+
+CREATE TABLE Ratings (
+    rating_id INT PRIMARY KEY AUTO_INCREMENT,
+    news_id INT,
+    ip_address VARCHAR(45) NOT NULL,
+    rating_value INT CHECK (rating_value >= 1 AND rating_value <= 5),
+    UNIQUE (news_id, ip_address),
+    FOREIGN KEY (news_id) REFERENCES News(news_id)
+);
