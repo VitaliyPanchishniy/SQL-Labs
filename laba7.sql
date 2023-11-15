a8d25f98b31cca6cab18474306f3802f1b93975a
VitaliyPanchishniy <144204960+VitaliyPanchishniy@users.noreply.github.com>
2023-11-15 09:03:38 +0200

    Update laba7.sql

--- laba7.sql
+++ laba7.sql
@@ -1,22 +1,35 @@
+
 CREATE TABLE categories (
     category_id INT PRIMARY KEY AUTO_INCREMENT,
-    category_name VARCHAR(50) NOT NULL
+    category_name VARCHAR(255) NOT NULL
 );
+
 
 CREATE TABLE news (
     news_id INT PRIMARY KEY AUTO_INCREMENT,
     category_id INT,
-    title VARCHAR(100) NOT NULL,
+    title VARCHAR(255) NOT NULL,
     content TEXT NOT NULL,
-    publication_date TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
-    FOREIGN KEY (category_id) REFERENCES Categories(category_id)
+    publication_time DATETIME NOT NULL,
+    FOREIGN KEY (category_id) REFERENCES categories(category_id)
 );
+
+
+CREATE TABLE comments (
+    comment_id INT PRIMARY KEY AUTO_INCREMENT,
+    news_id INT,
+    commenter_name VARCHAR(255) NOT NULL,
+    comment_text TEXT NOT NULL,
+    comment_time DATETIME NOT NULL,
+    FOREIGN KEY (news_id) REFERENCES news(news_id)
+);
+
 
 CREATE TABLE ratings (
     rating_id INT PRIMARY KEY AUTO_INCREMENT,
     news_id INT,
-    ip_address VARCHAR(45) NOT NULL,
-    rating_value INT CHECK (rating_value >= 1 AND rating_value <= 5),
-    UNIQUE (news_id, ip_address),
-    FOREIGN KEY (news_id) REFERENCES News(news_id)
+    ip_address VARCHAR(15) NOT NULL,
+    rating_value INT NOT NULL CHECK (rating_value BETWEEN 1 AND 5),
+    rating_time DATETIME NOT NULL,
+    FOREIGN KEY (news_id) REFERENCES news(news_id)
 );
