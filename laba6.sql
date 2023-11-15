fb42df732de97cfff3a9d5e3d572ea0f64004f8c
VitaliyPanchishniy <144204960+VitaliyPanchishniy@users.noreply.github.com>
2023-11-15 09:39:41 +0200

    Update laba6.sql

--- laba6.sql
+++ laba6.sql
@@ -9,29 +9,29 @@ WHERE c.region = r.uuid
 AND r.name = 'Nord';
 
 
-CREATE TABLE Cities (
+CREATE TABLE cities (
     city_id INT PRIMARY KEY,
     city_name VARCHAR(255)
 );
 
-CREATE TABLE MetroLines (
+CREATE TABLE metroLines (
     line_id INT PRIMARY KEY,
     city_id INT,
     line_name VARCHAR(255),
-    FOREIGN KEY (city_id) REFERENCES Cities(city_id)
+    FOREIGN KEY (city_id) REFERENCES cities(city_id)
 );
 
-CREATE TABLE Stations (
+CREATE TABLE stations (
     station_id INT PRIMARY KEY,
     line_id INT,
     station_name VARCHAR(255),
-    FOREIGN KEY (line_id) REFERENCES MetroLines(line_id)
+    FOREIGN KEY (line_id) REFERENCES metroLines(line_id)
 );
 
-CREATE TABLE Connections (
+CREATE TABLE connections (
     connection_id INT PRIMARY KEY,
     station_id_1 INT,
     station_id_2 INT,
-    FOREIGN KEY (station_id_1) REFERENCES Stations(station_id),
-    FOREIGN KEY (station_id_2) REFERENCES Stations(station_id)
+    FOREIGN KEY (station_id_1) REFERENCES stations(station_id),
+    FOREIGN KEY (station_id_2) REFERENCES stations(station_id)
 );
